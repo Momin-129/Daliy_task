d373323b4518d2c140a8b75ab8ef269931979c68
Momin Rather <momin.rather@gmail.com>
2023-04-24 08:53:01 +0530

    JavaScript Functions,Objects,Arrays

--- JavaScript_Basics/home.js
+++ JavaScript_Basics/home.js
@@ -1,2 +1,12 @@
-msg = "Hello World";
-showMessage(msg);
+let msg = "Hello World";
+let person = {
+  name: "Momin",
+  age: 23,
+  showInfo: function () {
+    showMessage(this.age);
+  },
+};
+
+const values = ["a", "b", "c"];
+const newValues = values.slice(1, 3);
+showMessage(values.indexOf("a"));
